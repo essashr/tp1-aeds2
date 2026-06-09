163d428a4e903d500613394f60ee6e2bf683e7d5
Arthur Emanoel <arthurcorreia839@gmail.com>
2026-06-09 14:24:56 -0300

    Criação da lista encadeada (pastas) e .h da patricia

---
+++ FUNCOES/PATRICIA/PATRICIA.h
@@ -0,0 +1,34 @@
+#ifndef PATRICIA_H
+#define PATRICIA_H
+#include <stdio.h>
+#include <stdlib.h>
+#include "././loader.h"
+#include "./LISTA_ENCADEADA/lista_encadeada.h"
+
+#define D 8
+typedef char TipoChave[MAX_PALAVRA];
+typedef unsigned char TipoIndexAmp;
+typedef unsigned char TipoDib;
+
+typedef enum {
+    Interno, Externo
+} TipoNo;
+
+typedef struct TipoPatNo* TipoArvore;
+typedef struct TipoPatNo {
+    TipoNo nt;
+    union
+    {
+        struct {
+            TipoIndexAmp Index;
+            TipoArvore Esq, Dir;
+        } NoInterno;
+        struct {
+            TipoChave Chave;
+            ListaDeIDs Lista;
+        } NoFolha;
+    } NO;
+    
+} TipoPatNo;
+
+#endif
